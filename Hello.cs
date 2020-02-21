70ca6c6e484c0832a5381350acdd1f7170e65848
Katy Solovewicz <katy@solovewicz.com>
2020-02-21 14:48:55 -0800

    add file setup

---
+++ Hello.cs
@@ -0,0 +1,9 @@
+using System;  // A directive that says what code is needed to run.
+
+class Program
+{
+  static void Main()  //Program code resides in the brackets under the static void Main() method within the Program class. This method is automatically run when we launch any C# program. Methods that automatically run when a program is launched are called entry points.
+  {
+    Console.WriteLine("Hello World");
+  }
+}
